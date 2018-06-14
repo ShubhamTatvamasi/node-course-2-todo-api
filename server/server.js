32b41525e5f1db686ed20cbd5c74eeb94025e29a
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-14 15:43:23 +0530

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ var app = express()
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
-  
   var todo = new Todo({
     text: req.body.text
   })
@@ -20,7 +19,14 @@ app.post('/todos', (req, res) => {
   }, (e) =>{
     res.status(400).send(e)
   })
+})
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(() => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e)
+  })
 })
 
 
