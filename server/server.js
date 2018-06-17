149fe546d2b756ab2badf1f7af6470faefc921d4
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-17 10:42:02 +0530

    modified:   package.json 	modified:   server/models/user.js 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('config/config')
+require('./config/config')
 
 const _ = require('lodash')
 const express = require('express')
@@ -98,6 +98,19 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send()
   })
 })
+
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+
+  user.save().then((user) => {
+    res.send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
+
+
 
 
 app.listen(port, () => {
